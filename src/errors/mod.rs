350a0bfc5eb91c8ee5e13c156bff3034a3e4cc4e
Nebojsa Jakovljevic <nebojsa@nebjak.net>
2025-01-11 18:46:15 +0100

    chore: format

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -41,15 +41,41 @@ impl IntoResponse for ApiError {
     fn into_response(self) -> Response {
         let (status, body) = match self {
             ApiError::BadRequest(_) => (StatusCode::BAD_REQUEST, "Bad Request".to_string()),
-            ApiError::NotFound(body) => (StatusCode::NOT_FOUND, body.unwrap_or("Not Found".to_string())),
-            ApiError::InternalServerError(body) => (StatusCode::INTERNAL_SERVER_ERROR, body.unwrap_or("Internal Server Error".to_string())),
-            ApiError::Unauthorized(body) => (StatusCode::UNAUTHORIZED, body.unwrap_or("Unauthorized".to_string())),
-            ApiError::Forbidden(body) => (StatusCode::FORBIDDEN, body.unwrap_or("Forbidden".to_string())),
-            ApiError::Conflict(body) => (StatusCode::CONFLICT, body.unwrap_or("Conflict".to_string())),
-            ApiError::TooManyRequests(body) => (StatusCode::TOO_MANY_REQUESTS, body.unwrap_or("Too Many Requests".to_string())),
-            ApiError::ServiceUnavailable(body) => (StatusCode::SERVICE_UNAVAILABLE, body.unwrap_or("Service Unavailable".to_string())),
-            ApiError::GatewayTimeout(body) => (StatusCode::GATEWAY_TIMEOUT, body.unwrap_or("Gateway Timeout".to_string())),
-            ApiError::Other(status, body) => (StatusCode::from_u16(status).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR), body.unwrap_or("Other Error".to_string())),
+            ApiError::NotFound(body) => (
+                StatusCode::NOT_FOUND,
+                body.unwrap_or("Not Found".to_string()),
+            ),
+            ApiError::InternalServerError(body) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                body.unwrap_or("Internal Server Error".to_string()),
+            ),
+            ApiError::Unauthorized(body) => (
+                StatusCode::UNAUTHORIZED,
+                body.unwrap_or("Unauthorized".to_string()),
+            ),
+            ApiError::Forbidden(body) => (
+                StatusCode::FORBIDDEN,
+                body.unwrap_or("Forbidden".to_string()),
+            ),
+            ApiError::Conflict(body) => {
+                (StatusCode::CONFLICT, body.unwrap_or("Conflict".to_string()))
+            }
+            ApiError::TooManyRequests(body) => (
+                StatusCode::TOO_MANY_REQUESTS,
+                body.unwrap_or("Too Many Requests".to_string()),
+            ),
+            ApiError::ServiceUnavailable(body) => (
+                StatusCode::SERVICE_UNAVAILABLE,
+                body.unwrap_or("Service Unavailable".to_string()),
+            ),
+            ApiError::GatewayTimeout(body) => (
+                StatusCode::GATEWAY_TIMEOUT,
+                body.unwrap_or("Gateway Timeout".to_string()),
+            ),
+            ApiError::Other(status, body) => (
+                StatusCode::from_u16(status).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR),
+                body.unwrap_or("Other Error".to_string()),
+            ),
         };
 
         (status, body).into_response()
